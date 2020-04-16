cd9be4276e66bd96f1716fdd0984f68859e50b64
akiraozaki1108 <61183197+akiraozaki1108@users.noreply.github.com>
2020-04-16 20:06:30 +0900

    訂正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws' 
-    config.fog_public = false
+    # config.fog_public = false
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
